a4f774d8164e453ecf35054e32b6fa05bcbe708d
Otoniel Reyes Galay <otonielreyesgalay@gmail.com>
2021-02-06 21:46:56 -0400

    Update reverser.ts

--- src/lib/reverser.ts
+++ src/lib/reverser.ts
@@ -7,6 +7,6 @@ export function reverser(str: string): string | null {
   if (typeof str === 'object') {
     return null;
   }
-  str = String(str);
+  const str = String(str);
   return str.split("").reverse().join("");
 }
